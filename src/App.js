4ff808b923a31d2cca35984b8d0d86cfc2d45cd0
christianturner <christianturner@hey.com>
2021-08-25 07:30:04 -0400

    subtotal component import to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import "./App.css";
+import Subtotal from "./components/Subtotal/Subtotal.jsx";
 import { Container } from "react-bootstrap";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <Container className="purchase-card">
-        <h1>Hey</h1>
-      </Container>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 100,
+    };
+  }
+  render() {
+    return (
+      <div className="container">
+        <Container className="purchase-card">
+          <Subtotal price={this.state.total.toFixed(2)} />
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
