fc823aa39af98d710610184671f0bf06ca70a716
christianturner <christianturner@hey.com>
2021-08-25 07:42:00 -0400

    savings comonent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import Subtotal from "./components/Subtotal/Subtotal.jsx";
-import { Container } from "react-bootstrap";
-import "./App.css";
 import { Component } from "react";
+import "./App.css";
+import { Container } from "react-bootstrap";
+import Subtotal from "./components/Subtotal/Subtotal.jsx";
+import PickupSavings from "./components/PickupSavings/PickupSavings.jsx";
 
 class App extends Component {
   constructor(props) {
@@ -9,13 +10,16 @@ class App extends Component {
 
     this.state = {
       total: 100,
+      PickupSavings: -3.85,
     };
   }
+
   render() {
     return (
       <div className="container">
         <Container className="purchase-card">
           <Subtotal price={this.state.total.toFixed(2)} />
+          <PickupSavings price={this.state.PickupSavings} />
         </Container>
       </div>
     );
---
+++ src/components/PickupSavings/PickupSavings.jsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import { Row, Col, Tooltip, OverlayTrigger } from "react-bootstrap";
+
+var styles = {
+  pickupSavings: {
+    textDecoration: "underline",
+  },
+  totalSavings: {
+    color: "red",
+    fontWeight: 800,
+  },
+};
+
+const tooltip = (
+  <Tooltip id="tooltip">
+    <p>
+      Picking up your order in the store helps cut costs, and we pass the
+      savings on to you.
+    </p>
+  </Tooltip>
+);
+
+export default class PickupSavings extends Component {
+  render() {
+    return (
+      <Row classname="show-grid">
+        <Col md={6}>
+          <OverlayTrigger placement="bottom" overlay={tooltip}>
+            <div style={styles.pickupSavings}> Pickup Savings </div>
+          </OverlayTrigger>
+        </Col>
+        <Col style={styles.totalSavings} md={6}>
+          {" "}
+          {`$${this.props.price}`}{" "}
+        </Col>
+      </Row>
+    );
+  }
+}
